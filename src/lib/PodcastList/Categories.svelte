d61805c6d923f8ae87fee42cda2ef72b15a7162e
Steven Bell <thebells1111@gmail.com>
2021-10-08 22:51:20 -0500

    mobile design

---
+++ src/lib/PodcastList/Categories.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let podcast;
+	export let handleSearch;
+</script>
+
+<div class="categories">
+	{#each Object.entries(podcast.categories)
+		.map(([key, value]) => value)
+		.concat(podcast.provider)
+		.filter((v) => v) as cat}
+		<span class="tag" on:click={handleSearch.bind(null, cat)}>{cat}</span>
+	{/each}
+</div>
+
+<style>
+	.tag {
+		padding: 2px 8px;
+		border-radius: 20px;
+		background-color: rgb(0, 117, 212);
+		color: rgb(245, 245, 245);
+		margin: 2px;
+		cursor: pointer;
+		display: inline-block;
+	}
+</style>
--- src/lib/PodcastList/PodcastListItem.svelte
+++ src/lib/PodcastList/PodcastListItem.svelte
@@ -1,78 +1,38 @@
 <script>
+	import Categories from './Categories.svelte';
 	export let podcast;
 	export let index;
 	export let handleSearch;
+	export let sectionWidth;
 </script>
 
 <div class="container">
 	<div class="row">
 		<div class="artwork">
-			<a href={'https://podcastindex.org/podcast/' + podcast.id}>
+			<a href={'https://curiocaster.com/#fastfollow-podcast:' + podcast.guid}>
 				<div class="image-container">
 					<img draggable="false" src={podcast.artwork || podcast.image} />
 				</div>
 			</a>
 		</div>
-		<div>
-			<a href={'https://podcastindex.org/podcast/' + podcast.id}>
+		<div class="data-container">
+			<a href={'https://curiocaster.com/#fastfollow-podcast:' + podcast.podcastGuid}>
 				<h2>{podcast.title}</h2>
 			</a>
 			<p>by {podcast.author}</p>
-			<div class="categories">
-				<div>
-					{#each Object.entries(podcast.categories)
-						.map(([key, value]) => value)
-						.concat(podcast.provider)
-						.filter((v) => v) as cat}
-						<span class="tag" on:click={handleSearch.bind(null, cat)}>{cat}</span>
-					{/each}
-				</div>
-			</div>
+			{#if sectionWidth > 649}
+				<Categories {handleSearch} {podcast} />
+			{/if}
 		</div>
 	</div>
-	<p>{podcast.description}</p>
+	{#if sectionWidth < 650}
+		<Categories {handleSearch} {podcast} />
+	{/if}
+	<p class="description">{podcast.description}</p>
 	<p class="index">{index + 1}</p>
 </div>
 
 <style>
-	.artwork {
-		display: inline-flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		padding: 15px;
-	}
-
-	img {
-		height: 140px;
-		width: 140px;
-		border-radius: 10px;
-		border: 1px solid #d7d7d7;
-	}
-
-	.row {
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-	}
-
-	h2 {
-		margin: 0 0 8px 0;
-	}
-
-	h4 {
-		margin: 0;
-		display: inline;
-	}
-
-	p {
-		margin: 0 0 8px 8px;
-	}
-
-	h4 + p {
-		display: inline;
-	}
-
 	.container {
 		background: #fff;
 		border: 1px solid #d6d6d6;
@@ -84,18 +44,70 @@
 		width: calc(100% - 40px);
 	}
 
+	.row {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.artwork {
+		display: inline-flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 15px;
+	}
+
+	.data-container {
+		width: calc(100% - 106px);
+	}
+
+	img {
+		height: 140px;
+		width: 140px;
+		border-radius: 10px;
+		border: 1px solid #d7d7d7;
+	}
+
+	h2 {
+		margin: 0 0 8px 0;
+	}
+
+	p {
+		margin: 0 0 8px 8px;
+	}
+
 	.index {
 		position: absolute;
 		bottom: 0;
 		right: 8px;
 	}
 
-	.tag {
-		padding: 2px 8px;
-		border-radius: 20px;
-		background-color: rgb(0, 117, 212);
-		color: rgb(245, 245, 245);
-		margin: 0 2px;
-		cursor: pointer;
+	@media (max-width: 649px) {
+		.container {
+			background: #fff;
+			border: 1px solid #d6d6d6;
+			border-radius: 6px;
+			margin: 10px 10px 10px 10px;
+			padding: 5px;
+			height: 470px;
+			position: relative;
+			width: calc(100% - 20px);
+		}
+		img {
+			height: 100px;
+			width: 100px;
+			margin-right: 6px;
+			border-radius: 6px;
+			border: 1px solid #d7d7d7;
+		}
+
+		.artwork {
+			padding: 0px;
+		}
+
+		.description {
+			margin-top: 4px;
+		}
 	}
 </style>
